774ec45fdd58c4ffa66f7319df011392ad247101
Julian Karrer <juliankarrer@t-online.de>
2025-11-06 13:42:43 +0100

    Add programm overview

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # Mörderischer Mai
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/87047277-bf19-4f8c-87c9-ac2e32b39ac2/deploy-status)](https://app.netlify.com/projects/krimimai/deploys)
+
 Offizielle Website für den Mörderischen Mai, das Allgäuer Krimi-Festival. 
 Gestaltet von Studio Erika, gebaut von Julian mit Gatsby, Decap CMS und Git-CI via Netlify.
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,85 @@
 import * as React from "react"
 import { useStaticQuery, graphql } from "gatsby"
-// import { Link } from "gatsby"
-// import { StaticImage } from "gatsby-plugin-image"
+import { Link } from "gatsby"
+import { getImage, GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
 
 import * as styles from "./index.module.css"
 
-const IndexPage = ({ children }) => {
+function ProgrammPunkt({ style, className, programmpunkt, bg_colour }) {
+  const portraits = programmpunkt.portrait_list.map((img) => {
+    return {
+      img: getImage(img.portrait_obj.portrait_image),
+      name: img.portrait_obj.portrait_name
+    }
+  })
+  const linkto = "/programm/" + programmpunkt.path.split("/").at(-1).split(".md").at(0)
+  console.log(programmpunkt, linkto)
+  return (
+    <div style={{ background: bg_colour, border: "var(--border)", ...style }} className={className}>
+      {/* portraits */}
+      <Link to={linkto}>
+        <div style={{ aspectRatio: 1 }}>
+          <div className="portraits-container">
+            {portraits.map(({ img, name }, i) => <GatsbyImage
+              image={img}
+              alt={name}
+              key={name + i}
+              className="portrait"
+            />)}
+          </div>
+        </div>
+        {/* first box: name of the event */}
+        <div style={{ borderTop: "var(--border)", padding: "var(--small-padding)" }}>
+          <h3>{programmpunkt.name}</h3>
+        </div>
+      </Link>
+      {/* second box: all the remaining info */}
+      <div style={{ borderTop: "var(--border)", padding: "var(--small-padding)" }}>
+        <span style={{ marginBottom: "50px" }}>{programmpunkt.author}</span>
+      </div>
+    </div >
+  )
+}
+
+const IndexPage = () => {
   const data = useStaticQuery(graphql`
-    query DateQuery {
+    query IndexPageQuery {
       contentYaml {
         date
+        bg_colour_subpage
+      }
+      allMarkdownRemark {
+        nodes {
+          fileAbsolutePath
+          frontmatter {
+            author
+            beginn
+            einlass
+            name
+            title
+            ticketlink
+            portrait_list {
+              portrait_obj {
+                portrait_name
+                portrait_image {
+                  childImageSharp {
+                    gatsbyImageData
+                  }
+                }
+              }
+            }
+          }
+        }
       }
     }
-    `)
-  const { contentYaml } = data
-  const { date } = contentYaml
+`)
+  const { contentYaml, allMarkdownRemark } = data
+  const { date, bg_colour_subpage } = contentYaml
+  const { nodes } = allMarkdownRemark
+  const programmpunkte = nodes.map(({ frontmatter, fileAbsolutePath }) => ({ path: fileAbsolutePath, ...frontmatter }))
 
   return (
     <Layout>
@@ -45,12 +107,15 @@ const IndexPage = ({ children }) => {
       </div >
 
       {/* programm */}
-      <div className="container">
+      <div className="bordered">
         <h1 style={{ borderBottom: "var(--border)" }}>Programm</h1>
         <p style={{ padding: "var(--inner-padding)" }}>
           Der mörderische Mai hat einiges zu bieten.<br />
           Folgende Tatorte sind geplant:
         </p>
+        <div className="programm-grid">
+          {programmpunkte.map((punkt) => <ProgrammPunkt programmpunkt={punkt} bg_colour={bg_colour_subpage} />)}
+        </div>
       </div>
 
 
